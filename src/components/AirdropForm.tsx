cc17426b20306bf1f8633d9f1dc63ca76196e562
GoldTeaaa <handayariawan@gmail.com>
2025-06-01 19:46:50 +0700

    Basic Form Framework

--- src/components/AirdropForm.tsx
+++ src/components/AirdropForm.tsx
@@ -1,7 +1,42 @@
+"use client"
+
+import { useState } from "react";
+import InputField from "@/ui/InputField";
+
 export default function AidropForm(){
+    const [tokenAddress, setTokenAddress] = useState("");
+    const [recipients, setRecipients] = useState("");
+    const [amount, setAmount] = useState("");
+
+    async function handleSubmit() {
+        console.log(tokenAddress);
+    }
+
     return(
         <div>
-            <h1>HI</h1>
+            <InputField
+                label="Token Address"
+                placeholder="0x"
+                value={tokenAddress}
+                onChange={e => setTokenAddress(e.target.value)}
+            />
+            <InputField
+                label="Recipients"
+                placeholder="0x1234,0x123431"
+                value={recipients}
+                onChange={e => setRecipients(e.target.value)}
+                large={true}
+            />
+            <InputField
+                label="Amount"
+                placeholder="100.200.300"
+                value={amount}
+                onChange={e => setAmount(e.target.value)}
+                large={true}
+            />
+            <button onClick={handleSubmit}>
+                Submit
+            </button>
         </div>
     );
 }
